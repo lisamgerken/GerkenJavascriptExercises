50d23d211dff2036bc41c1670ee4f5e2f2070fbc
Lisa Gerken <lisamgerken@yahoo.com>
2017-10-09 21:29:32 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+/*
+Exercise 1
+var userInput = prompt("Please enter a number");
+var sum = 0
+for (var i = 0; i < userInput; i++){
+	sum += i + 1;
+}
+console.log("This is the sum of all numbers plus yours " + sum)
+*/
+
+
+
+/*Exercise 2
+
+var userInput = prompt("Do you want to play?");
+var allWords = ""
+do {
+	var userWord = prompt("Enter a word");
+	allWords = allWords + " " + userWord
+	userInput = prompt("Do you want to play again?")
+}while (userInput != "no");
+console.log(allWords);
+*/
+
+
+/*Exercise 3
+
+var name = prompt("What is your name?");
+var userInput = ""
+var counter = 0
+var exclaim = ""
+while (userInput != "no") {
+	userInput = prompt("Would you like to print your name?")
+	console.log("Hello.  My name is " + name + exclaim)
+	exclaim += "!"
+	}
+	 
+
+
+/*Exercise 4
+
+var userInput = prompt("Is it morning, noon, or evening?");
+var am = "morning"
+var nooN = "noon"
+var pm = "evening"
+
+if (userInput == am) {
+	console.log("Since it's morning, you should be eating breakfast.  We suggest eggs and toast.")
+} else if (userInput == nooN) {
+	console.log("Since its noon, you should be eating lunch.  We suggest a salad.")
+} else {
+	console.log("Since it's evening, you should be eating dinner.  We suggest chicken and rice.")
+}
+*/
+
+
+
